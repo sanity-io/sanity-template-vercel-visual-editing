3361b62e36138c0133cf95b34eaac1a3d436a2b5
Cody Olsen <stipsan@gmail.com>
2023-04-27 03:24:05 +0200

    feat: add youtube embed

--- sanity.config.ts
+++ sanity.config.ts
@@ -13,6 +13,7 @@ import project from 'schemas/documents/project'
 import duration from 'schemas/objects/duration'
 import milestone from 'schemas/objects/milestone'
 import timeline from 'schemas/objects/timeline'
+import youtube from 'schemas/objects/youtube'
 import home from 'schemas/singletons/home'
 import settings from 'schemas/singletons/settings'
 
@@ -36,6 +37,7 @@ export default defineConfig({
       // Objects
       milestone,
       timeline,
+      youtube,
     ],
   },
   plugins: [
