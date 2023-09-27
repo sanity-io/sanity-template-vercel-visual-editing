e6ff9abbe4b2005dc737de9ca20ed43b76f76292
Cody Olsen <stipsan@gmail.com>
2023-09-27 12:28:41 +0200

    fix: use padding instead in the footer

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -3,10 +3,10 @@ import { CustomPortableText } from 'components/shared/CustomPortableText'
 
 export function Footer({ footer }: { footer: PortableTextBlock[] }) {
   return (
-    <footer className="bottom-0 w-full bg-white py-12 text-center md:py-20">
+    <footer className="bottom-0 w-full bg-white px-12 py-12 text-center md:py-20">
       {footer && (
         <CustomPortableText
-          paragraphClasses="text-md md:text-xl inline-block"
+          paragraphClasses="text-md md:text-xl"
           value={footer}
         />
       )}
